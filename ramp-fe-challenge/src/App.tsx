50c8367d494899317a51aa55e8912d85b5f10a3e
Sugandha Kumar <Sugandha.kumar03@gmail.com>
2024-08-09 16:07:01 -0700

    bug 4 down

--- ramp-fe-challenge/src/App.tsx
+++ ramp-fe-challenge/src/App.tsx
@@ -81,7 +81,7 @@ export function App() {
           {transactions !== null && (
             <button
               className="RampButton"
-              disabled={paginatedTransactionsUtils.loading}
+              disabled={paginatedTransactionsUtils.loading || paginatedTransactions?.nextPage === null}
               onClick={async () => {
                 await loadAllTransactions()
               }}
